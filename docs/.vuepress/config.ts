718d32140438540e4b437abec967c24cb67ad4b2
mkflow27 <mkflow27@gmail.com>
2024-04-02 10:05:00 +0200

    docs: Docs are for V3

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -21,7 +21,7 @@ export default defineUserConfig({
   locales: {
     '/': {
       lang: 'en-US',
-      title: 'Balancer',
+      title: 'Balancer V3',
       description: 'Learn, integrate, and build on a programmable AMM',
     },
   },
